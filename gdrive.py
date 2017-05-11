a9d379f5a66261de5af9ec1381e0e846f4efda93
Vasily Rozhnikov <shiawasena.hoshi@gmail.com>
2017-05-12 00:50:24 +0300

    - testing google drive uploading

---
+++ gdrive.py
@@ -0,0 +1,24 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
+drive = GoogleDrive(gauth)
+
+textfile = drive.CreateFile()
+textfile.SetContentFile('/mnt/Content/Combs A. - Python Machine Learning Blueprints - 2016.pdf')
+textfile.Upload()
+print("finish")
