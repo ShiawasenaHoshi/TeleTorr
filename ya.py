b5ef9150eb4a1082a7229aa72f2372ccb69c2ed5
Vasily Rozhnikov <shiawasena.hoshi@gmail.com>
2017-05-12 22:03:01 +0300

    - add yadisk rest-client. Thanks to Hixon10 https://github.com/Hixon10/simple-rest-yandex-disk-client

---
+++ ya.py
@@ -0,0 +1,12 @@
+from yadisk.YandexDiskRestClient import YandexDiskRestClient
+
+
+def uploadAndGetLink(filename, path, token):
+    #todo recursive upload folders
+    client = YandexDiskRestClient(token)
+    try:
+        link = client.get_download_link_to_file("/" + filename)
+    except:
+        client.upload_file(path + filename, "/" + filename)
+        link = client.get_download_link_to_file("/" + filename)
+    return link
