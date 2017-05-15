fb793e95138857a7a3be02cba631d1b77d0c035a
Vasily Rozhnikov <shiawasena.hoshi@gmail.com>
2017-05-15 15:27:30 +0300

    - refactoring;

--- telegram_torrent.py
+++ telegram_torrent.py
@@ -14,19 +14,10 @@ from transmission import TransmissionAgent
 
 CONFIG_FILE = 'setting.json'
 
+
 class Torrenter(telepot.helper.ChatHandler):
-    YES = '<OK>'
-    NO = '<NO>'
-    MENU_HOME = 'HOME'
-    MENU_SEARCH = 'SEARCH TORRENT'
-    MENU_INPUT_WORD = 'INPUT A WORD'
-    MENU_CHOOSE_TORRENT = 'CHOOSE AN ITEM'
-    MENU_TORRENT_LIST = 'TORRENT LIST'
-    MENU_START = 'START'
-    MENU_STOP = 'STOP'
-    MENU_REMOVE = 'REMOVE'
     rssUrl = """https://torrentkim1.net/bbs/rss.php?k="""
-    GREETING = "SELECT MENU"
+
     global scheduler
     DownloadFolder = ''  # Option: Input your subtitle location to save subtitle files,
 
@@ -40,24 +31,100 @@ class Torrenter(telepot.helper.ChatHandler):
     def open(self, initial_msg, seed):
         self.menu()
 
+    def on_close(self, exception):
+        pass
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        # Check ID
+        if not chat_id in validUsers:
+            print("Permission Denied")
+            return
+
+        if content_type is 'text':
+            self.handle_command(msg['text'])
+            return
+
+        if content_type is 'document':
+            file_name = msg['document']['file_name']
+            if file_name[-7:] == 'torrent':
+                file_id = msg['document']['file_id']
+                self.handle_seedfile(file_id)
+                return
+            self.sender.sendMessage('Invalid File')
+            return
+
+        self.sender.sendMessage('Invalid File')
+
+    def handle_command(self, command):
+        if command == self.MENU_HOME:
+            self.menu()
+        elif command == self.MENU_SEARCH:
+            self.tor_get_keyword()
+        elif command == self.MENU_TORRENT_LIST:
+            self.tor_show_list()
+        elif self.mode == self.MENU_INPUT_WORD:  # Get Keyword
+            self.tor_search(command)
+        elif self.mode == self.MENU_CHOOSE_TORRENT:
+            self.tor_download(command)
+        elif self.mode == self.MENU_TORRENT_LIST and command == self.MENU_MAN_CMD:
+            self.tor_help()
+        elif self.mode == self.MENU_TORRENT_LIST and command.lower().startswith("/"):
+            self.tor_list_cmd(command)
+
+    def handle_seedfile(self, file_id):
+        try:
+            self.sender.sendMessage('Saving torrent file..')
+            generated_file_path = self.DownloadFolder + \
+                                  "".join(random.sample(string.ascii_letters, 8)) + ".torrent"
+            bot.download_file(file_id, generated_file_path)
+            self.agent.download(generated_file_path)
+            os.system("rm " + generated_file_path)
+            if not scheduler.get_jobs():
+                scheduler.add_job(self.agent.check_torrents,
+                                  'interval', minutes=1)
+        except Exception as inst:
+            self.sender.sendMessage('ERORR: {0}'.format(inst))
+            return
+        self.sender.sendMessage('Start Downloading')  # todo send filename
+
+    # ---------- Concrete commands ------------
+    GREETING = "Select menu"
+    MENU_HOME = 'Home'
+    MENU_SEARCH = 'Search torrent'
+    MENU_INPUT_WORD = 'Type a word'
+    MENU_CHOOSE_TORRENT = 'Choose an item'
+    MENU_TORRENT_LIST = 'Torrent list'
+    MENU_MAN_CMD = "Help"
+    MENU_START = '/st'
+    MENU_STOP = '/sp'
+    MENU_REMOVE = '/rm'
+
     def menu(self):
         mode = ''
         show_keyboard = {'keyboard': [
             [self.MENU_SEARCH], [self.MENU_TORRENT_LIST], [self.MENU_HOME]]}
         self.sender.sendMessage(self.GREETING, reply_markup=show_keyboard)
 
-    def yes_or_no(self, comment):
-        show_keyboard = {'keyboard': [[self.YES, self.NO], [self.MENU_HOME]]}
-        self.sender.sendMessage(comment, reply_markup=show_keyboard)
-
     def tor_get_keyword(self):
         self.mode = self.MENU_INPUT_WORD
         self.sender.sendMessage('Enter a Keyword')
 
-    def put_menu_button(self, l):
-        menulist = [self.MENU_HOME]
-        l.append(menulist)
-        return l
+    def tor_show_list(self):
+        self.mode = self.MENU_TORRENT_LIST
+        result = self.agent.getCurrentList()
+        if not result:
+            self.sender.sendMessage('The torrent list is empty')
+            self.menu()
+            return
+        outList = self.agent.parseList(result)
+        for e in outList:
+            self.sender.sendMessage(self.agent.printElement(e))
+
+        if outList.__len__() > 0:
+            show_keyboard = {'keyboard': [
+                [self.MENU_SEARCH], [self.MENU_TORRENT_LIST], [self.MENU_MAN_CMD], [self.MENU_HOME]]}
+            self.sender.sendMessage(self.GREETING, reply_markup=show_keyboard)
 
     def tor_search(self, keyword):
         self.mode = ''
@@ -84,6 +151,11 @@ class Torrenter(telepot.helper.ChatHandler):
                                 reply_markup=show_keyboard)
         self.mode = self.MENU_CHOOSE_TORRENT
 
+    def put_menu_button(self, l):
+        menulist = [self.MENU_HOME]
+        l.append(menulist)
+        return l
+
     def tor_download(self, selected):
         self.mode = ''
         index = int(selected.split('.')[0]) - 1
@@ -95,113 +167,23 @@ class Torrenter(telepot.helper.ChatHandler):
             scheduler.add_job(self.agent.check_torrents, 'interval', seconds=5)
         self.menu()
 
-    def tor_show_list(self):
-        self.mode = self.MENU_TORRENT_LIST
-        # self.sender.sendMessage('Let me check the torrent list..')
-        result = self.agent.getCurrentList()
-        if not result:
-            self.sender.sendMessage('The torrent list is empty')
-            self.menu()
-            return
-        outList = self.agent.parseList(result)
-        for e in outList:
-            self.sender.sendMessage(self.agent.printElement(e))
+    def tor_help(self):
         show_keyboard = {'keyboard': [
-            [self.MENU_SEARCH], [self.MENU_HOME]]}
-        if outList.__len__() > 0:
-            self.sender.sendMessage("type stop/start/remove 1/1,2,n/all", reply_markup=show_keyboard)
+            [self.MENU_SEARCH], [self.MENU_TORRENT_LIST], [self.MENU_MAN_CMD], [self.MENU_HOME]]}
+        self.sender.sendMessage("Commands:\n"
+                                "/st 1 - start torrent with id 1\n"
+                                "/sp 2,3 - stop torrents with id 2 and 3\n"
+                                "/rm all - remove all torrents", reply_markup=show_keyboard)
 
-    def tor_start(self, command):
-        out = self.agent.start(command.lower().replace(self.MENU_START.lower() + " ", ""))
-        # self.sender.sendMessage(out)
+    def tor_list_cmd(self, cmd):
+        cmd = cmd.lower()
+        if cmd.startswith(self.MENU_START.lower()):
+            self.agent.start(cmd.replace(self.MENU_START.lower() + " ", ""))
+        elif cmd.startswith(self.MENU_STOP.lower()):
+            self.agent.stop(cmd.replace(self.MENU_STOP.lower() + " ", ""))
+        elif cmd.startswith(self.MENU_REMOVE.lower()):
+            self.agent.remove(cmd.replace(self.MENU_REMOVE.lower() + " ", ""))
         self.tor_show_list()
-
-    def tor_stop(self, command):
-        out = self.agent.stop(command.lower().replace(self.MENU_STOP.lower() + " ", ""))
-        # self.sender.sendMessage(out)
-        self.tor_show_list()
-
-    def tor_remove(self, command):
-        out = self.agent.remove(command.lower().replace(self.MENU_REMOVE.lower() + " ", ""))
-        # self.sender.sendMessage(out)
-        self.tor_show_list()
-
-
-
-    def handle_command(self, command):
-        if command == self.MENU_HOME:
-            self.menu()
-        elif command == self.MENU_SEARCH:
-            self.tor_get_keyword()
-        elif command == self.MENU_TORRENT_LIST:
-            self.tor_show_list()
-        elif self.mode == self.MENU_INPUT_WORD:  # Get Keyword
-            self.tor_search(command)
-        elif self.mode == self.MENU_CHOOSE_TORRENT:  # Download Torrent
-            self.tor_download(command)
-        elif self.mode == self.MENU_TORRENT_LIST and command.lower().startswith(
-                self.MENU_START.lower()):  # Download Torrent
-            self.tor_start(command)
-        elif self.mode == self.MENU_TORRENT_LIST and command.lower().startswith(
-                self.MENU_STOP.lower()):  # Download Torrent
-            self.tor_stop(command)
-        elif self.mode == self.MENU_TORRENT_LIST and command.lower().startswith(
-                self.MENU_REMOVE.lower()):  # Download Torrent
-            self.tor_remove(command)
-
-    def handle_smifile(self, file_id, file_name):
-        try:
-            self.sender.sendMessage('Saving subtitle file..')
-            bot.download_file(file_id, self.DownloadFolder + file_name)
-        except Exception as inst:
-            self.sender.sendMessage('ERORR: {0}'.format(inst))
-            return
-        self.sender.sendMessage('Done')
-
-    def handle_seedfile(self, file_id, file_name):
-        try:
-            self.sender.sendMessage('Saving torrent file..')
-            generated_file_path = self.DownloadFolder + \
-                                  "".join(random.sample(string.ascii_letters, 8)) + ".torrent"
-            bot.download_file(file_id, generated_file_path)
-            self.agent.download(generated_file_path)
-            os.system("rm " + generated_file_path)
-            if not scheduler.get_jobs():
-                scheduler.add_job(self.agent.check_torrents,
-                                  'interval', minutes=1)
-        except Exception as inst:
-            self.sender.sendMessage('ERORR: {0}'.format(inst))
-            return
-        self.sender.sendMessage('Start Downloading')
-
-    def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        # Check ID
-        if not chat_id in validUsers:
-            print("Permission Denied")
-            return
-
-        if content_type is 'text':
-            self.handle_command(msg['text'])
-            return
-
-        if content_type is 'document':
-            file_name = msg['document']['file_name']
-            if file_name[-3:] == 'smi':
-                file_id = msg['document']['file_id']
-                self.handle_smifile(file_id, file_name)
-                return
-            if file_name[-7:] == 'torrent':
-                file_id = msg['document']['file_id']
-                self.handle_seedfile(file_id, file_name)
-                return
-            self.sender.sendMessage('Invalid File')
-            return
-
-        self.sender.sendMessage('Invalid File')
-
-    def on_close(self, exception):
-        pass
 
 
 def parseConfig(filename):
@@ -222,6 +204,7 @@ def getConfig(config):
     validUsers = config['common']['valid_users']
     downloadPath = config['common']['download_path']
     yaDiskToken = config["yadisk"]['token']
+
 
 config = parseConfig(CONFIG_FILE)
 if not bool(config):
